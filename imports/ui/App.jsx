9cd91b7e0db81815f8eb4c70349a62ba93bd13f8
Askar Karasaev <askar.karasaev@gmail.com>
2017-02-13 16:22:35 +0900

    Adding locations with a form

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Locations } from '../api/locations.js';
@@ -6,6 +7,21 @@ import Location from './Location.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+  handleSubmit(event) {
+  event.preventDefault();
+
+  // Find the text field via the React ref
+  const name = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+  Locations.insert({
+    name,
+    createdAt: new Date(), // current time
+  });
+
+  // Clear form
+  ReactDOM.findDOMNode(this.refs.textInput).value = '';
+}
+
   renderLocations() {
     return this.props.locations.map((location) => (
       <Location key={location._id} location={location} />
@@ -16,7 +32,14 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Locations List</h1>
+          <h1>Locations</h1>
+          <form className="new-location" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new location"
+            />
+          </form>
         </header>
 
         <ul>
@@ -33,6 +56,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    locations: Locations.find({}).fetch(),
+    locations: Locations.find({}, { sort: { createdAt: -1 } } ).fetch(),
   };
 }, App);
